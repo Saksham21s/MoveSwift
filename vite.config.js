7c61b4300fbeca70b78847eb37903590a796774d
Saksham  Pandey <saksham4200@gmail.com>
2025-02-15 11:27:57 +0530

    Updated Vite-config.js

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  server: {
-    port: 5173, 
-  },
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: 'index.html',
+      input: 'index.html'
     }
-  }
+  },
+  server: {
+    port: 5173,
+  },
 });
